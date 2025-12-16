ed4d7587d3a69712547dc324cf51ab54782d9ffc
screak95 <128321007+screak95@users.noreply.github.com>
2025-12-16 22:05:12 +0200

    Updated unit_tests.cpp with add function unit tests

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,1 +1,15 @@
+#include "math_operations.h"
+#include <gtest/gtest.h>
 
+// Unit test for add function
+TEST(MathOperationsTest, AddFunction) {
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(-1, 1), 0);
+    EXPECT_EQ(add(0, 0), 0);
+    EXPECT_EQ(add(-5, -5), -10);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
